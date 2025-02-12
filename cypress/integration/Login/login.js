902355befe4e29fb5945e2f063273a2726891558
Rogattites-ino <rosselynn.gattites@ino-games.com>
2025-02-12 12:30:13 -0300

    Corrigindo erro - removendo node_modules do repositório

---
+++ cypress/integration/Login/login.js
@@ -0,0 +1,20 @@
+import { Given, When, And, Then} from "cypress-cucumber-preprocessor/steps";
+
+Given('A user enters to the login page', ()=> {
+    cy.visit('/')
+})
+When('A user enters the username {string}', (username)=> {
+    cy.get('#user-name').type(username)
+})
+And('A user enters the password {string}',(password) => {
+    cy.get('#password').type(password)
+})
+And('A user enters the password {string}',(password)=> {w
+    cy.get('#password').type(password)
+})
+And('A user clicks on the login button',(password)=> {
+    cy.get('#login-button').click()
+})
+Then('A user will be logged in', ()=>{
+    cy.url().should('contains','/inventory.html')
+})
